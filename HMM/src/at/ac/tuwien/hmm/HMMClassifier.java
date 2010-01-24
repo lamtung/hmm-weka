12b05bb2d4d74a07ceb0735f834324497c7bbd69
nltung <nltung@2444436c-0679-11df-8659-3787339302e4>
2010-01-24 18:16:36 +0000

    Change some minor things

--- HMM/src/at/ac/tuwien/hmm/HMMClassifier.java
+++ HMM/src/at/ac/tuwien/hmm/HMMClassifier.java
@@ -64,7 +64,7 @@ public class HMMClassifier extends RandomizableClassifier {
 	    
 	    //train the HMMs
 	    //handler.train(data, m_Variations);
-	    handler.trainWithTabuSearch(data, 20);	    
+	    handler.trainWithTabuSearch(data, 100);	    
 	    System.out.println("building done");
 	    
 	}
