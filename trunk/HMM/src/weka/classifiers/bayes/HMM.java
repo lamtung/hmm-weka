7ef4fce25ca18a70d0cd0705b1f9486dae1c2d69
cschmidt.vie <cschmidt.vie@2444436c-0679-11df-8659-3787339302e4>
2010-01-26 19:24:20 +0000

    Added a wrapper so it is visible in the weka GUI without tampering with the original jar

---
+++ trunk/HMM/src/weka/classifiers/bayes/HMM.java
@@ -0,0 +1,13 @@
+package weka.classifiers.bayes;
+
+/** 
+ * Wrapper for Weka so the class is visible in the GUI.
+ * 
+ * @author Christof Schmidt
+ *
+ */
+public class HMM extends at.ac.tuwien.hmm.HMMClassifier {
+
+	static final long serialVersionUID = -3481068294659183100L;
+
+}
